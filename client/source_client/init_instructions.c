d56e58393038fb7d96df6cfd03b24496f1b9691a
Lucas Lemoigne <lemoig_l@localhost.localdomain>
2018-06-12 16:12:55 +0200

    init_instructions Add Instructions

--- client/source_client/init_instructions.c
+++ client/source_client/init_instructions.c
@@ -9,9 +9,14 @@
 
 void		init_instructions(t_instructions *instru)
 {
-	instru->_instructions =;
-/*		&forward, &right, &left,
-		&look, &inventory, &bro,
-		&broadcast, &connect, &fork,
-		&eject, &take, &set, &incantation;*/
+	instru->_instructions = 
+		{
+			&forward, &right, &left,
+			&look, &inventory, &bro,
+			&broadcast, &connect, &fork,
+			&eject, &take, &set, &incantation,
+			&map_size, &content_tile, &content_map,
+			&name_all_teams, &player_pos, &player_lvl,
+			&player_inventory, &time_unit_request, &time_unit_modif
+		}
 }
