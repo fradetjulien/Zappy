92ca1aad80351adf4189a88d8c1efdd003a9499b
simon <francois.simon@epitech.eu>
2018-06-23 10:47:16 +0200

    Command take finished

--- srv/src/commands/take.c
+++ srv/src/commands/take.c
@@ -1,65 +1,82 @@
 #include "server.h"
 
-void	check_on_map_first(t_map *tmp, server *server, char **params, int j)
+static int	check_on_map_first(t_map *tmp, server *server, char **params, int i)
 {
-	if (strcmp("food\n", params[j]) == 0 &&
-	    tmp->map->rsrc->nb_food > 0) {
-		tmp->map->rsrc->nb_food--;
-		server->client->time++;
+	if (strcmp("food\n", params[1]) == 0 &&
+	    tmp->rsrc->nb_food > 0) {
+		tmp->rsrc->nb_food--;
+		server->client[i].time += 126;
+		server->client[i].inventory->nb_food += 126;
+		dprintf(server->client[i].fd, "ok\n");
+		return (1);
 	}
-	if (strcmp("linemate\n", params[j]) == 0 &&
-	    tmp->map->rsrc->nb_linemate > 0) {
-		tmp->map->rsrc->nb_food--;
-		server->client->time++;
+	if (strcmp("linemate\n", params[1]) == 0 &&
+	    tmp->rsrc->nb_linemate > 0) {
+		tmp->rsrc->nb_food--;
+		server->client[i].inventory->nb_linemate++;
+        dprintf(server->client[i].fd, "ok\n");
+        return (1);
 	}
-	if (strcmp("deraumere\n", params[j]) == 0 &&
-	    tmp->map->rsrc->nb_deraumere > 0) {
-		tmp->map->rsrc->nb_deraumere--;
-		server->client->time++;
-	}
-	if (strcmp("sibur\n", params[j]) == 0 &&
-	    tmp->map->rsrc->nb_sibur > 0) {
-		tmp->map->rsrc->nb_sibur--;
-		server->client->time++;
-	}
+	return (0);
 }
 
-void	check_on_map_second(t_map *tmp, server *server, char **params, int j)
+static int	check_on_map_second(t_map *tmp, server *server, char **params, int i)
 {
-	if (strcmp("mendiane\n", params[j]) == 0 &&
-	    tmp->map->rsrc->nb_mendiane > 0) {
-		tmp->map->rsrc->nb_mendiane--;
-		server->client->time++;
+	if (strcmp("mendiane\n", params[1]) == 0 &&
+	    tmp->rsrc->nb_mendiane > 0) {
+		tmp->rsrc->nb_mendiane--;
+        server->client[i].inventory->nb_mendiane++;
+        dprintf(server->client[i].fd, "ok\n");
+        return (1);
 	}
-	if (strcmp("phiras\n", params[j]) == 0 &&
-	    tmp->map->rsrc->nb_phiras > 0) {
-		tmp->map->rsrc->nb_phiras--;
-		server->client->time++;
+	if (strcmp("phiras\n", params[1]) == 0 &&
+	    tmp->rsrc->nb_phiras > 0) {
+		tmp->rsrc->nb_phiras--;
+        server->client[i].inventory->nb_phiras++;
+        dprintf(server->client[i].fd, "ok\n");
+        return (1);
 	}
-	if (strcmp("thystmane\n", params[j]) == 0 &&
-	    tmp->map->rsrc->nb_thystmane > 0) {
-		tmp->map->rsrc->nb_thystmane--;
-		server->client->time++;
+	if (strcmp("thystame\n", params[1]) == 0 &&
+	    tmp->rsrc->nb_thystame > 0) {
+		tmp->rsrc->nb_thystame--;
+        server->client[i].inventory->nb_thystame++;
+        dprintf(server->client[i].fd, "ok\n");
+        return (1);
 	}
+	return (0);
+}
+
+static int    check_on_map_third(t_map *tmp, server *server, char **params, int i)
+{
+    if (strcmp("deraumere\n", params[1]) == 0 &&
+        tmp->rsrc->nb_deraumere > 0) {
+        tmp->rsrc->nb_deraumere--;
+        server->client[i].inventory->nb_deraumere++;
+        dprintf(server->client[i].fd, "ok\n");
+        return (1);
+    }
+    if (strcmp("sibur\n", params[1]) == 0 &&
+        tmp->rsrc->nb_sibur > 0) {
+        tmp->rsrc->nb_sibur--;
+        server->client[i].inventory->nb_sibur++;
+        dprintf(server->client[i].fd, "ok\n");
+        return (1);
+    }
+    return (0);
 }
 
 void take(server *server, int i, char **params)
 {
 	t_map	*tmp = server->map;
-	int		j = 0;
 
-	while (params[j])
-		j++;
-	if (j < 4 || j > 9)
-		dprintf(server->client[i].fd, "ko\n");
-	else {
-		while (tmp) {
-			if (tmp->node_pos->x == server->posX &&
-			    tmp->node_pos->y == server->posY) {
-				check_on_map_first(tmp, server, params, j);
-				check_on_map_second(tmp, server, params, j);
-			}
-			tmp = tmp->next;
-		}
+	while (tmp) {
+	    if (tmp->node_pos->x == server->client[i].posX &&
+            tmp->node_pos->y == server->client[i].posY) {
+	        if (check_on_map_first(tmp, server, params, i) == 0 &&
+	            check_on_map_second(tmp, server, params, i) == 0 &&
+                check_on_map_third(tmp, server, params, i) == 0)
+	            dprintf(server->client[i].fd, "ko\n");
+        }
+	    tmp = tmp->next;
 	}
 }
