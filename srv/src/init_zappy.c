0b815fa475d2b9514b892322a00b6797959d5091
Lucas Lemoigne <lemoig_l@localhost.localdomain>
2018-06-24 14:11:31 +0200

    check on args

--- srv/src/init_zappy.c
+++ srv/src/init_zappy.c
@@ -7,7 +7,7 @@
 
 #include "server.h"
 
-int	verification_args(char **av)
+int verification_args(char **av)
 {
 	if (strcmp(av[1], "-p") != 0 ||
 	    strcmp(av[3], "-x") != 0 ||
@@ -19,20 +19,59 @@ int	verification_args(char **av)
 
 int	fill_port(char **av)
 {
+	int	k = 0;
+	
+	for (i = 0; i < ac; i++) {
+		if (strcmp(av[i], "-p") == 0 && strcmp(av[i + 1], "-x") != 0) {
+			k = 1;
+			for (j = 0; av[i + 1][j]; j++) {
+				if (av[i + 1][j] < "0" || av[i + 1][j] > "9" ||
+				    av[i + 1][j] != ".")
+					return 4242;
+			}
+		}
+	}
+	if (i == ac && k == 0)
+		return 4242;
 	return (atoi(av[2]));
 }
 
 int	fill_width(char **av)
 {
+	int	k = 0;
+	
+	for (i = 0; i < ac; i++) {
+		if (strcmp(av[i], "-x") == 0 && strcmp(av[i + 1], "-y") != 0) {
+			k = 1;
+			for (j = 0; av[i + 1][j]; j++) {
+				if (av[i + 1][j] < "0" || av[i + 1][j] > "9")
+					return 20;
+			}
+		}
+	}
+	if (i == ac && k == 0)
+		return 20;
 	return (atoi(av[4]));
 }
 
 int	fill_height(char **av)
 {
+	int	k = 0;
+	
+	for (i = 0; i < ac; i++) {
+		if (strcmp(av[i], "-y") == 0 && strcmp(av[i + 1], "-n") != 0) {
+			for (j = 0; av[i + 1][j]; j++) {
+				if (av[i + 1][j] < "0" || av[i + 1][j] > "9")
+					return 20;
+			}
+		}
+	}
+	if (i == ac && k == 0)
+		return 20;
 	return (atoi(av[6]));
 }
 
-team	*fill_name_teams(int ac, char **av)
+team *fill_name_teams(int ac, char **av)
 {
 	team *list = NULL;
 
@@ -46,33 +85,26 @@ team	*fill_name_teams(int ac, char **av)
 
 int	fill_nb_players(int ac, char **av) 
 {
-	int i = 0;
+	int	k = 0;
 	for (i = 8; i < ac; ++i) {
-		if (strcmp(av[i], "-c") == 0)
-			break;
+		if (strcmp(av[i], "-c") == 0 && strcmp(av[i + 1], "-f") != 0) {
+			k = 1;
+			for (j = 0; av[i + 1][j]; j++) {
+				if (av[i + 1][j] < "0" || av[i + 1][j] > "9")
+					return 6;
+			}
+		}
 	}
+	if (i == ac && k == 0)
+		return 6;
 	return (atoi(av[i + 1]));
 }
 
 int	fill_max_client(int ac, char **av)
 {
-	int i = 0;
-
 	while (i < ac) {
 		if (strcmp(av[i], "-n") == 0)
 			break ;
-	}
-	return (atoi(av[i + 1]));
-}
-
-int	fill_frequency(int ac, char **av)
-{
-	int i = 0;
-
-	while (i < ac) {
-		if (strcmp(av[i], "-f") == 0)
-			break ;
-		i++;
 	}
 	return (atoi(av[i + 1]));
 }
